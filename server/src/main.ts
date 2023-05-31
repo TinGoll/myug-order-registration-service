3d7722e941b953d45cccbacd9498ba79157ee760
TinGoll <roma.oliynyk83@gmail.com>
2023-05-31 17:21:44 +0300

    Configuration setting

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const httpsOptions = getHttpsOptions();
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
+  const config = app.get(ConfigService);
+  const port = config.get<number>('API_PORT') || 3000;
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (origin) {
+        callback(null, true);
+      } else {
+        console.log('blocked cors for:', origin);
+        callback(null, true);
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe, Authorization, access-control-allow-origin',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(port, () => console.log(`server started on port: ${port}`));
 }
 bootstrap();
+
+function getHttpsOptions() {
+  try {
+    return {
+      key: fs.readFileSync('/etc/letsencrypt/live/facade-good.ru/privkey.pem'),
+      cert: fs.readFileSync(
+        '/etc/letsencrypt/live/facade-good.ru/fullchain.pem',
+      ),
+    };
+  } catch (error) {
+    console.log('Ошибка получения ключей:', error.message);
+  }
+}
