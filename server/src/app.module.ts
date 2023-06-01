08afd1a47a70ddaa36b6fab2d136f0c2d574b272
TinGoll <roma.oliynyk83@gmail.com>
2023-06-01 17:03:10 +0300

    setting db

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,34 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `../.env`,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: config.get<'postgres'>('TYPEORM_CONNECTION'),
+        host: config.get<string>('TYPEORM_HOST'),
+        username: config.get<string>('TYPEORM_USERNAME'),
+        password: config.get<string>('TYPEORM_PASSWORD'),
+        database: config.get<string>('TYPEORM_DATABASE'),
+        port: config.get<number>('TYPEORM_PORT'),
+        entities: [__dirname + config.get<string>('TYPEORM_ENTITIES')],
+        migrations: [__dirname + config.get<string>('TYPEORM_MIGRATIONS')],
+        migrationsTableName: config.get<string>(
+          'TYPEORM_MIGRATIONS_TABLE_NAME',
+        ),
+        synchronize: Boolean(config.get<string>('TYPEORM_SYNCHRONIZE')),
+        autoLoadEntities: true,
+        logging: ['error', 'warn', 'query'],
+      }),
     }),
   ],
   controllers: [AppController],
