a8a931894796962abaa437b634f43a340d4b53d0
TinGoll <roma.oliynyk83@gmail.com>
2023-06-16 17:07:51 +0300

    add polymorf cmp

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,11 +3,22 @@ import type { HeadFC, PageProps } from "gatsby";
 import Layout from "../layouts/Layout";
 import { Container } from "@mui/material";
 
+type Props<T extends React.ElementType> = {
+  component?: T;
+  children?: React.ReactNode;
+} & React.ComponentPropsWithoutRef<T>;
+
+function MYComponent<T extends React.ElementType>({ component, children, ...props }: Props<T>): React.JSX.Element {
+  const Component = component || "div";
+  return <Component {...props}>{children}</Component>;
+}
+
 const IndexPage: React.FC<PageProps> = ({ uri }) => {
   return (
     <Layout headerSticky sx={{}}>
       <Container>
-       
+        Lorem ipsum dolor sit amet.
+        <MYComponent  component="button" >Привет</MYComponent>
       </Container>
     </Layout>
   );
